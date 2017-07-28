90fb3bf1342c73384674e83add5afbf80faf7864
Chandra <526688@MACPCAIM00168.local>
2017-07-28 12:53:01 +0530

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    puts params
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:email, :password, :password_confirmation, :street_address, :first_name, :last_name, :zip_code])
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  def name
+    [first_name, last_name].compact.join(" ")
+  end
+
+  def address
+    adr = [street_address, state, country].compact.join(", ")
+    adr += ", P.O: #{zip_code}" if zip_code.present?
+    adr
+  end
+end
